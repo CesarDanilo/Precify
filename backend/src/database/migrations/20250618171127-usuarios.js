6b185ad490cbfd6925c94bf58dc116fdb33ae2fd
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-06-18 14:27:32 -0300

    created migration usuarios

--- backend/src/database/migrations/20250618171127-usuarios.js
+++ backend/src/database/migrations/20250618171127-usuarios.js
@@ -2,21 +2,53 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        allowNull: false,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      senha: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      plano_id: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'ativo', // ou outro valor padrão
+      },
+      tentativas_gratis_restantes: {
+        type: Sequelize.INTEGER,
+        defaultValue: 3,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('NOW()'),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('NOW()'),
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
   }
 };

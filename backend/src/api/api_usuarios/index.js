51b8ae20d1ae334ae2014912491ad29c6e4001d5
CSR <billypalacios616@gmail.com>
2025-06-25 17:56:38 -0300

    feat: criando rotas para post [postUsers]

--- backend/src/api/api_usuarios/index.js
+++ backend/src/api/api_usuarios/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const getUsers = require('../../controllers/Usuarios/getUsers');
+const postUsers = require('../../controllers/Usuarios/postUsers');
 
 router.get('/getUsers', getUsers);
+router.post('/postUsers', postUsers);
 
 module.exports = router;

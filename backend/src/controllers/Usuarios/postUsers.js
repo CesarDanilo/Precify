4faf0e89499dc9a672145770f06db264b9ebf6b2
CSR <billypalacios616@gmail.com>
2025-06-25 17:07:15 -0300

    feat: validando entrada de dados com zod

--- backend/src/controllers/Usuarios/postUsers.js
+++ backend/src/controllers/Usuarios/postUsers.js
@@ -3,12 +3,28 @@ const { z } = require('zod');
 
 module.exports = async function postUsers(req, res) {
     try {
+        const nomeSchema = z.string().min(4, 'Nome é obrigatório');
+        const emailSchema = z.string().email('Email inválido');
+        const senhaSchema = z.string().min(6, 'Senha deve ter pelo menos 6 caracteres');
+
+        const usuarioSchema = z.object({
+            nome: nomeSchema,
+            email: emailSchema,
+            senha: senhaSchema
+        })
+
         const { nome, email, senha } = res.body;
 
         if (!nome || !email || !senha) {
             return `❌ DADOS ENVIADOS INCORRETOS: NOME:${nome} | EMAIL:${email} | SENHA:${senha}`
         }
+        const result = usuarioSchema.safeParse({ nome: nome, email: email, senha: senha });
 
+        if (!result.success) {
+            console.log('❌ ERROS NA VALIDAÇÃO:', resultado.error.format());
+        } else {
+            console.log('✅ DADOS VALIDADOS COM SÚCESSO:', resultado.data);
+        }
 
         console.log('✅ USUARIOS GRAVADOS COM SÚCESSO')
     } catch (error) {

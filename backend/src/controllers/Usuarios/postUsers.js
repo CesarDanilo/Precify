60715c921d285da52069829d73701a3fd57ba186
CSR <billypalacios616@gmail.com>
2025-06-25 16:08:23 -0300

    feat: 1 455 048 516

--- backend/src/controllers/Usuarios/postUsers.js
+++ backend/src/controllers/Usuarios/postUsers.js
@@ -1,1 +1,17 @@
-const { z } = require('zod')
+const { Usuarios } = require('../../database/models/');
+const { z } = require('zod');
+
+module.exports = async function postUsers(req, res) {
+    try {
+        const { nome, email, senha } = res.body;
+
+        if (!nome || !email || !senha) {
+            return `❌ DADOS ENVIADOS INCORRETOS: NOME:${nome} | EMAIL:${email} | SENHA:${senha}`
+        }
+
+
+        console.log('✅ USUARIOS GRAVADOS COM SÚCESSO')
+    } catch (error) {
+        console.log('❌ ERRO NA TENTATIVA DE GRAVAR USUARIOS:', error);
+    }
+}

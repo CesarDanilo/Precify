2348666e745e58448199863e19987f4bc2fc80bb
CSR <billypalacios616@gmail.com>
2025-06-27 11:11:09 -0300

    feat: funcionalidade middleware de validação de usuarios ativa

--- backend/src/api/api_usuarios/index.js
+++ backend/src/api/api_usuarios/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const getUsers = require('../../controllers/Usuarios/getUsers');
 const postUsers = require('../../controllers/Usuarios/postUsers');
 
+const validacaoUsuariosExistente = require('../../middlewares/Usuarios/validacaoUsuarioExistente')
+
 router.get('/getUsers', getUsers);
-router.post('/postUsers', postUsers);
+router.post('/postUsers', validacaoUsuariosExistente, postUsers);
 
 module.exports = router;

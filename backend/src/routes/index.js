22f6db4b4534450bb420d3cdd1cf6dbacb85eaec
CSR <billypalacios616@gmail.com>
2025-06-27 14:17:38 -0300

    chore: adiconando rotas

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const api_usuarios = require('../api/api_usuarios');
+const api_planos = require('../api/api_planos');
 
-router.use('/usuarios', api_usuarios); 
+router.use('/usuarios', api_usuarios);
+router.use('/planos', api_planos);
 
 module.exports = router;    

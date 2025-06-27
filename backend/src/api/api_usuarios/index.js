35e8f36a028145b05585ef3fb9d4845f31285578
CSR <billypalacios616@gmail.com>
2025-06-27 12:14:46 -0300

    feat: finalizado [deleteUsers.js]

--- backend/src/api/api_usuarios/index.js
+++ backend/src/api/api_usuarios/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const getUsers = require('../../controllers/Usuarios/getUsers');
 const postUsers = require('../../controllers/Usuarios/postUsers');
+const deleteUsers = require('../../controllers/Usuarios/deleteUsers');
 
 const validacaoUsuariosExistente = require('../../middlewares/Usuarios/validacaoUsuarioExistente')
 
 router.get('/getUsers', getUsers);
 router.post('/postUsers', validacaoUsuariosExistente, postUsers);
+router.delete('/deleteUsers/:id', deleteUsers);
 
 module.exports = router;

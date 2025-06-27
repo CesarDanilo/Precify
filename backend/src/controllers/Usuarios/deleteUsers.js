702902739e3233856d093ba8aa600641da454bb6
CSR <billypalacios616@gmail.com>
2025-06-27 11:55:09 -0300

    feat: adicionado um novo arquivo [deleteUsers.js]

---
+++ backend/src/controllers/Usuarios/deleteUsers.js
@@ -0,0 +1,25 @@
+const { where } = require('sequelize');
+const { Usuarios } = require('../../database/models/');
+
+module.exports = async function (req, res) {
+    const { id } = req.params;
+
+    try {
+        const result = await Usuarios.findByPk(id);
+
+        if (!result) {
+            return res.status(404).json({ msg: `ERRO! USUÁRIO COM ID: ${id} NÃO EXISTE!` })
+        }
+
+        try {
+            const usuarioDeletado = await Usuarios.destroy({ where: { id } });
+        } catch (error) {
+            return res.status(400).json({ msg: "ERRO NA TENTANTIVA DE DELETAR USUÁRIO APOS VALIDAR QUE USUÁRIO EXISTE!", erro: error });
+        }
+
+        return res.status(200).json({ msg: '✅ USUÁRIO EXCLUIDO COM SÚCESSO!' });
+
+    } catch (error) {
+        return res.status(400).json({ msg: "ERRO NA TENTANTIVA DE DELETAR USUÁRIO", erro: error });
+    }
+}

821ac2f92dd718d441b6933544f7f19c55b0b1d4
CSR <billypalacios616@gmail.com>
2025-06-28 10:05:19 -0300

    feat: created getProdutos.js

---
+++ backend/src/controllers/Produtos/getProdutos.js
@@ -0,0 +1,25 @@
+const { Produtos } = require('../../database/models/');
+
+module.exports = async function getProdutos(req, res) {
+    const { id, nome } = req.query;
+
+    try {
+        const result = await Produtos.findByPk(id);
+        if (!result) return res.status(500).json({ msg: `ESSE PRODUTOS COM ID [${id}] NÃO EXISTE!: ${error}` });
+
+        let where = {};
+        if (id) where.id = id;
+        if (nome) where.nome = { [Op.iLike]: `%${nome}%` };
+
+        const listaProdutos = await Produtos.findAll({
+            where,
+            attributes: ['id', 'nome',]
+        });
+        if (listaProdutos.length === 0) return res.status(200).json({ msg: `NÃO POSSUI PRODUTOS CADASTRADOS!` });
+
+        return res.status(200).json({ msg: `✅ PRODUTO LISTADO`, data: listaProdutos })
+
+    } catch (error) {
+        return res.status(500).json({ msg: `NÃO FOI POSSIVEL LISTAR OS PRODUTOS: ${error}` })
+    }
+}

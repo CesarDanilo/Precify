ac06617bac6d06436965a6d489eda375f2b7376c
CSR <billypalacios616@gmail.com>
2025-06-28 11:19:39 -0300

    chore: resolvendo bug getProdutos.js

--- backend/src/controllers/Produtos/getProdutos.js
+++ backend/src/controllers/Produtos/getProdutos.js
@@ -5,9 +5,6 @@ module.exports = async function getProdutos(req, res) {
     const { id, nome } = req.query;
 
     try {
-        const result = await Produtos.findByPk(id);
-        if (!result) return res.status(500).json({ msg: `ESSE PRODUTOS COM ID [${id}] NÃO EXISTE!: ${error}` });
-
         let where = {};
         if (id) where.id = id;
         if (nome) where.nome = { [Op.iLike]: `%${nome}%` };
@@ -16,11 +13,14 @@ module.exports = async function getProdutos(req, res) {
             where,
             attributes: ['id', 'nome', 'descricao', 'preco', 'imagem', 'createdAt', 'updatedAt']
         });
-        if (listaProdutos.length === 0) return res.status(200).json({ msg: `NÃO POSSUI PRODUTOS CADASTRADOS!` });
 
-        return res.status(200).json({ msg: `✅ PRODUTO LISTADO`, data: listaProdutos })
+        if (listaProdutos.length === 0) {
+            return res.status(404).json({ msg: `❌ NENHUM PRODUTO ENCONTRADO COM OS CRITÉRIOS INFORMADOS.` });
+        }
+
+        return res.status(200).json({ msg: `✅ PRODUTOS ENCONTRADOS`, data: listaProdutos });
 
     } catch (error) {
-        return res.status(500).json({ msg: `NÃO FOI POSSIVEL LISTAR OS PRODUTOS: ${error}` })
+        return res.status(500).json({ msg: `❌ ERRO AO LISTAR PRODUTOS`, error: error.message });
     }
-}
+};

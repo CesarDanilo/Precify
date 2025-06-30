75b4d2f598f520b8093c88b12703e9e70ed72c17
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-06-30 14:43:35 -0300

    chore: finalizando a api produtos

--- backend/src/api/api_produtos/index.js
+++ backend/src/api/api_produtos/index.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const getProdutos = require('../../controllers/Produtos/getProdutos');
 const postProdutos = require('../../controllers/Produtos/postProdutos');
 const deleteProdutos = require('../../controllers/Produtos/deleteProdutos');
-// const updateProdutos = require('../../controllers/Produtos/updateProdutos');
+const updateProdutos = require('../../controllers/Produtos/updateProdutos');
 
 router.get('/getProdutos', getProdutos);
 router.post('/postProdutos', postProdutos);
 router.delete('/deleteProdutos/:id', deleteProdutos);
-// router.put('/updateProdutos/:id', updateProdutos);
+router.put('/updateProdutos/:id', updateProdutos);
 
 module.exports = router;

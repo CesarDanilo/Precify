539e703774e9e1c9a393ad1f9497da0b87aecaca
CSR <billypalacios616@gmail.com>
2025-06-30 12:07:48 -0300

    feat: finalizando rotas delete produtos

--- backend/src/api/api_produtos/index.js
+++ backend/src/api/api_produtos/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const getProdutos = require('../../controllers/Produtos/getProdutos');
 const postProdutos = require('../../controllers/Produtos/postProdutos');
-// const deleteProdutos = require('../../controllers/Produtos/deleteProdutos');
+const deleteProdutos = require('../../controllers/Produtos/deleteProdutos');
 // const updateProdutos = require('../../controllers/Produtos/updateProdutos');
 
 router.get('/getProdutos', getProdutos);
 router.post('/postProdutos', postProdutos);
-// router.delete('/deleteProdutos/:id', deleteProdutos);
+router.delete('/deleteProdutos/:id', deleteProdutos);
 // router.put('/updateProdutos/:id', updateProdutos);
 
 module.exports = router;

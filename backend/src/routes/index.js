a0d768e0fa024efd91ef214a196d25249cc0b315
CSR <billypalacios616@gmail.com>
2025-07-01 09:50:45 -0300

    feat: finalizando rotas favoritos

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const api_usuarios = require('../api/api_usuarios');
 const api_planos = require('../api/api_planos');
 const api_produtos = require('../api/api_produtos');
+const api_favoritos = require('../api/api_favoritos');
 
 router.use('/usuarios', api_usuarios);
 router.use('/planos', api_planos);
 router.use('/produtos', api_produtos);
+router.use('/favoritos', api_favoritos);
 
 module.exports = router;    

56379b1475b06499dba077a504e8a32ea164f978
CSR <billypalacios616@gmail.com>
2025-07-04 17:46:49 -0300

    chore: criando rota deleteFavoritos

--- backend/src/api/api_favoritos/index.js
+++ backend/src/api/api_favoritos/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const getFavoritos = require('../../controllers/Favoritos/getFavoritos');
 const postFavoritos = require('../../controllers/Favoritos/postFavoritos');
-// const deleteFavoritos = require('../../controllers/Favoritos/deleteFavoritos');
+const deleteFavoritos = require('../../controllers/Favoritos/deleteFavoritos');
 const updateFavoritos = require('../../controllers/Favoritos/updateFavoritos');
 
 router.get('/getFavoritos', getFavoritos);
 router.post('/postFavoritos', postFavoritos);
-// router.delete('/deleteFavoritos/:id', deleteFavoritos);
+router.delete('/deleteFavoritos/:id', deleteFavoritos);
 router.put('/updateFavoritos/:id', updateFavoritos);
 
 module.exports = router;

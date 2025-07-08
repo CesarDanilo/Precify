4d4fdc28350ec7d7decaa0490b2ab45271ec5b4d
CSR <billypalacios616@gmail.com>
2025-07-08 12:50:10 -0300

    chore: implementando formulario

--- frontend/src/admin/components/Dialogs/UserCreateDialog.jsx
+++ frontend/src/admin/components/Dialogs/UserCreateDialog.jsx
@@ -1,3 +1,13 @@
 export function UserCreateDialog() {
+    return (
+        <div className="fixed inset-0 bg-opacity-30 backdrop-blur-sm flex items-center justify-center z-50">
+            <div className="bg-gray-800/70 backdrop-blur-md border border-white/20 w-1/2 h-1/2 p-6 rounded-2xl shadow-xl text-white">
+                <h2 className="text-xl mb-4">Create User</h2>
+                <form>
+                    {/* Seu formulário aqui */}
+                </form>
+            </div>
+        </div>
 
+    )
 }
--- frontend/src/admin/pages/UsersContent.jsx
+++ frontend/src/admin/pages/UsersContent.jsx
@@ -1,9 +1,11 @@
 import { ButtonAdd } from "../components/buttons/buttonsAdd";
 import { TableComponent } from "../components/Table/TableComponent";
+import { UserCreateDialog } from "../components/Dialogs/UserCreateDialog";
 
 export function UsersContent() {
     return (
         <div className="flex flex-col items-center justify-center w-full px-4">
+            <UserCreateDialog />
             <div className="flex w-full justify-end mb-3">
                 <ButtonAdd />
             </div>

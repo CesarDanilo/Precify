58e092e17e04af41b0df40c298fafbbb91ab8cf3
CSR <billypalacios616@gmail.com>
2025-07-08 14:49:09 -0300

    add onClone function

--- frontend/src/admin/components/Sidebar.jsx
+++ frontend/src/admin/components/Sidebar.jsx
@@ -11,14 +11,14 @@ export function Sidebar({ setSelectItem }) {
     ]
     return (
         <div>
-            <nav className="bg-black text-white h-screen w-64">
+            <nav className="bg-gray-500/20 text-gray-800 h-screen w-64">
                 <div className="p-4">
                     <h1 className="text-xl font-bold">Admin Panel</h1>
                 </div>
                 <ul className="mt-4">
                     {links.map(link => (
-                        <li key={link.name} onClick={() => setSelectItem(link.name)} className="px-4 py-2 hover:bg-blue-700 cursor-default">
-                            <div className="flex items-center text-white">
+                        <li key={link.name} onClick={() => setSelectItem(link.name)} className="px-4 py-2 hover:bg-blue-700 text-gray-800 hover:text-white cursor-default ">
+                            <div className="flex items-center ">
                                 <link.icon className="w-5 h-5 mr-2" />
                                 {link.label}
                             </div>

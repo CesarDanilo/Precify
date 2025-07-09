0453adf9aa76f220206a7ca408ed0dc019d6a247
CSR <billypalacios616@gmail.com>
2025-07-09 10:44:38 -0300

    feat: functionFetchPlanos.js

---
+++ frontend/src/admin/functions/functionFetchPlanos.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export function functionFetchPlanos() {
+    try {
+        return axios.get('http://localhost:4444/api/planos/getPlanos')
+            .then(response => {
+                if (response.status === 200) {
+                    return response.data;
+                } else {
+                    throw new Error("Erro ao buscar planos");
+                }
+            });
+    } catch (error) {
+        console.error("Erro ao buscar planos:", error);
+        throw new Error("Não foi possível buscar os planos. Tente novamente mais tarde.");
+    }
+}

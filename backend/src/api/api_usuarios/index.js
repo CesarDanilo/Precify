4bf5b26fd18865d9f3d0da1dbf2ab8255ec05985
CSR <billypalacios616@gmail.com>
2025-07-18 12:29:47 -0300

    feat: criando nova rota de autenticação de usuarios

--- backend/src/api/api_usuarios/index.js
+++ backend/src/api/api_usuarios/index.js
@@ -5,10 +5,12 @@ const postUsers = require('../../controllers/Usuarios/postUsers');
 const deleteUsers = require('../../controllers/Usuarios/deleteUsers');
 const updateUsers = require('../../controllers/Usuarios/updateUsers');
 
-const validacaoUsuariosExistente = require('../../middlewares/Usuarios/validacaoUsuarioExistente')
+const validacaoUsuariosExistente = require('../../middlewares/Usuarios/validacaoUsuarioExistente');
+const validacaoUsuarioLogin = require('../../middlewares/Usuarios/ValidacaoUsuarioLogin');
 
 router.get('/getUsers', getUsers);
 router.post('/postUsers', validacaoUsuariosExistente, postUsers);
+router.post('/validationUser', validacaoUsuarioLogin);
 router.delete('/deleteUsers/:id', deleteUsers);
 router.put('/updateUsers/:id', updateUsers);
 
---
+++ backend/src/middlewares/Usuarios/ValidacaoUsuarioLogin.js
@@ -0,0 +1,42 @@
+import { Usuarios } from '../../database/models/';
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+module.exports = async function validacaoUsuarioLogin(req, res, next) {
+    const { email, senha } = req.body;
+
+    if (!email || !senha) {
+        return res.status(400).json({ msg: "Email e senha são obrigatórios!" });
+    }
+
+    try {
+        const user = await Usuarios.findOne({ where: { email: email } });
+        if (!user) {
+            return res.status(404).json({ msg: "Usuário não encontrado!" });
+        }
+
+        const checkPassword = await bcrypt.compare(senha, user.senha);
+        if (!checkPassword) {
+            console.log("Senha incorreta");
+            return res.status(422).json({ msg: "Senha incorreta!" });
+        }
+
+        // Gera o token JWT
+        const secret = process.env.SECRET;
+        if (!secret) {
+            return res.status(500).json({ msg: "Secret key not defined!" });
+        }
+
+        const token = jwt.sign({ id: user.id }, secret, { expiresIn: '1h' });
+
+        const dados = { email: user.email, name: user.nome, userId: user.id };
+
+        return res.status(200).json({ msg: "Autenticação realizada com sucesso!", token, dados });
+
+    } catch (erro) {
+        // Em caso de erro, retorna uma resposta genérica
+        console.error(erro);
+        return res.status(500).json({ msg: "Erro interno no servidor!" });
+    }
+
+}

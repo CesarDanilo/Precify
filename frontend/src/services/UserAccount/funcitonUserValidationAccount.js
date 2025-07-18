98673ca3f381e68b112a1d2a90732aeed99600ef
CSR <billypalacios616@gmail.com>
2025-07-18 12:16:47 -0300

    crinado function para validar meu usuario

---
+++ frontend/src/services/UserAccount/funcitonUserValidationAccount.js
@@ -0,0 +1,9 @@
+import axios from axios;
+
+export default async function functionUserValidationAccount() {
+    try {
+        
+    } catch (error) {
+        console.log(`NÃO FOI POSSIVEL VALIDAR O USUARIO! ${error}`)
+    }
+}
